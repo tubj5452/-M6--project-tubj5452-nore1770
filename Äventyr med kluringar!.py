a55843f3965edcf196742b35e1a24243dab473af
tubj5452 <113420778+tubj5452@users.noreply.github.com>
2022-10-19 11:45:30 +0200

    Add files via upload
    På god väg med story, "grenar" kvar att lägga till + "beskrivande"/korrekt story (inledning etc).

--- Äventyr med kluringar!.py
+++ Äventyr med kluringar!.py
@@ -8,17 +8,16 @@ yesNo = ["yes", "no"]
 equations = {"x^3 = 8, what is x":"2", "What is the derivative of 1/x":"-1/(x^2)", "Simplify 76/8":"19/2", "Expand (x + 3)^2":"x^2+6x+9"}
 options = ["restart", "quit"]
 riddles = {"What has to be broken before you can use it?" : "egg" , "I am tall when I am young and short when I am old. What am I?" : "candle"}
-sentences = ["a","b","c"]#["I like bananas", "Programming is hard but fun!", "In my free-time i only study :("]
+sentences = ["I like bananas", "Programming is hard but fun!", "In my free-time i only study :("] #["a","b","c"]#["I like bananas", "Programming is hard but fun!", "In my free-time i only study :("]
+
 
 sumPoints = 0
-
-
 player = input("Your name: ")
 
 
 
-def math(prompt, equations):
-    print(f"\nTry to solve this question to complete this {prompt}\n\nYou have 3 guesses")
+def math(situation):
+    print(f"\nTry to solve this question to {situation}\n\nYou have 3 guesses")
     equation = random.choice(list(equations))
     print(equation)
      
@@ -31,7 +30,7 @@ def math(prompt, equations):
             return points
         elif points == 1:
             points == 0
-            return menu(f"Incorrect!\nGame over!\n", options, sumPoints) 
+            return "gameOver"
         else:
             points -= 1
             print(f"Incorrect! You have {points} guess(es) left")
@@ -41,24 +40,23 @@ def askRiddle(situation):
     riddle = random.choice(list(riddles))
     print(riddle)
     points = 3
-   
         
     while points <= 3:
         playerAnswer = checkAnswer(input("Answer: "))
 
         if playerAnswer == riddles[riddle]:
-            print(f"Correct! You have earned {points} point(s)")
+            print(f"Correct! You have earned {points} point(s) and completed this {situation}")
             riddles.pop(riddle)
             return points
         elif points == 1:
             points == 0
-            return menu(f"Incorrect!\nGame over!\n", options, sumPoints) 
+            return "gameOver"
         else:
             points -= 1
             print(f"Incorrect! You have {points} guess(es) left")   
             
-def binaryCode(prompt):
-    print(f"You have to decode this binary code to {prompt}. \nGood luck!\n")
+def binaryCode(situation):
+    print(f"You have to decode this binary code to {situation}. \nGood luck!\n")
     enter = True
     index = 0
     random = ""
@@ -73,11 +71,11 @@ def binaryCode(prompt):
     while points <= 3:
         playerAnswer = checkAnswer(input("Answer: "))
         if playerAnswer == str(answer):
-            print(f"Correct! You have earned {points} point(s)")
+            print(f"Correct! You have earned {points} point(s) and managed to {situation}")
             return points
         elif points == 1:
             points == 0
-            return menu(f"Incorrect!\nGame over!\n", options, sumPoints) 
+            return "gameOver"
         else:
             points -= 1
             print(f"Incorrect! You have {points} guess(es) left")        
@@ -88,8 +86,51 @@ def binaryCodeAnswer(random):
     for n in random:
         if n == '1':
             answer = answer + 2**(index)
-        index = index - 1
+        index -= 1
     return answer
+
+
+def speedGame(situation):
+    start(f"This is a speed-game. You need to write the sentence within 10 seconds to {situation}\nYou have 3 tries and the time restarts\n Are you ready?", yesNo)  
+    sentence = random.choice(list(sentences))
+    print(f"\nYour sentence is: {sentence}\n")
+    sentence = checkAnswer(sentence)
+    
+    s = 5
+    points = 3
+    
+    print("Your time starts in: ")
+    while s >= 0:
+ 
+        # Timer represents time left on countdown
+        timer = datetime.timedelta(seconds = s)   
+        # Prints the time left on the timer
+        print(timer, end="\r")#----------------------------------------- EXPERIMENTERA MED
+        # Delays the program one second
+        time.sleep(1)
+        # Reduces total time by one second
+        s -= 1
+        
+    print("")  
+    
+    
+    while True:
+        t1 = time.time()
+        playerAnswer = checkAnswer(input("Answer here: "))
+        t2 = time.time()
+        t = t2 - t1
+    
+        if t > 10:
+            sumPoints = 0
+            return "gameOverTime"
+        elif playerAnswer == sentence:
+            print(f"Correct! You have earned {points} point(s)")
+            return points
+        elif points == 1:
+            return "gameOverIncorrect"
+        else:
+            points -= 1
+            print(f"Incorrect! You have {points} guess(es) left")        
             
 def menu(prompt, options, sumPoints):
     printList(f"{prompt}\nTotal points earned: {sumPoints}\n\nWhat do you want to do?\n", options)
@@ -99,9 +140,7 @@ def menu(prompt, options, sumPoints):
         if action == "quit":
             break
         elif action == "restart":
-            sumPoints = 0
-            return start("Good luck, are you ready?", yesNo)
-            
+            return action  
             
 def start(prompt, strings):
     printList(f"{prompt}", yesNo)
@@ -137,86 +176,125 @@ def checkAnswer(playerInput):
     
 
 
-def speedGame(prompt, sentences):
-    start(f"This is a speed-game. To succed you need to write the sentence within 10 seconds to {prompt}\nYou have 3 tries and the time restarts\n Are you ready?", yesNo)  
-    sentence = random.choice(list(sentences))
-    s = 5
-    points = 3
+  
+#Vissa gameOver vill vi bara ska restarta det specifika spelet, medan andra blir total gameOver. 
+def main(sumPoints):
     
-    print("Your time starts in: ")
-    while s >= 0:
- 
-        # Timer represents time left on countdown
-        timer = datetime.timedelta(seconds = s)   
-        # Prints the time left on the timer
-        print(timer, end="\r")
-        # Delays the program one second
-        time.sleep(1)
-        # Reduces total time by one second
-        s -= 1
-        
-    print("")
-    
-    print(f"\nYour sentence is: {sentence}\n")
-    
-    while True:
-        t1 = time.time()
-        playerAnswer = checkAnswer(input("Answer here: "))
-        t2 = time.time()
-        t = t2 - t1
-    
-        if t > 5:
-            sumPoints = 0
-            return menu(f"To slow!\nGame over!\n", options, sumPoints)
-        elif playerAnswer == sentence:
-            print(f"Correct! You have earned {points} point(s)")
-            return points
-        elif points == 1:
-            menu(f"Wrong answer\nGame over!\n", options, sumPoints)
-            sumPoints = 0
-        else:
-            points -= 1
-            print(f"Incorrect! You have {points} guess(es) left")        
-        
+    if start(f"Welcome {player}! Are you ready for your adventure?", yesNo) == "playerIsReady":
+        print("Introduction to story")
+        print("You meet the kingdom's fairy to recieve directions, but its not for free...")
 
-def checkForRestart(sumPoints, result):
-    if result == "playerIsReady": 
-            return "restart"
+        while True:
+            result = math("get the directions ")
+            if result == "gameOver":
+                print("You are out of guesses - the fairy is kind enough to give you a second chance")
+                result = askRiddle("get another chance")
+                if result == "gameOver":
+                    print("This is your last chance - the fairy is getting angry at you stupidity")
+                    result = speedGame("survive")
+                    if result == "gameOverTime" or result == "gameOverIncorrect":
+                        menu(f"To slow!\nThe fairy is furious...\nGame over!\n", options, sumPoints)
+                        return False
+                    else:
+                        True
+                else:
+                    True
+            else:
+                sumPoints = result
+                print("Congratulations! You receved directions from the fairy")
+                print(f"\nTotal points earned so far: {sumPoints}")
+                break
+
+    print("continue story(to the library)")
+    result = binaryCode("open the door and enter the library")
+    if result == "gameOver":
+        menu(f"Incorrect!\nGame over!\n", options, sumPoints)
+        return False
     else:
         sumPoints = sumPoints + result
+        print("Congratulations! You managed to open the door")
         print(f"\nTotal points earned so far: {sumPoints}")
-        return sumPoints
 
-#OMSTARTSFUNKTIONEN MÅSTE FIXAS       
-def main(sumPoints):
-    if start(f"Welcome {player}! Are you ready for your adventure?", yesNo) == "playerIsReady":
-        math("situation", equations)
-        result = checkForRestart(sumPoints, math("situation", equations))
-        if result == "restart": 
-            return False
+    print("continue story(in the library)")
+    result = speedGame("find the correct book before the angry librarian comes back")   
+    if result == "gameOverTime":
+        menu(f"To slow!\nThe angry librarian found you...\nGame over!\n", options, sumPoints)
+        return False
+    elif result == "gameOverIncorrect":
+        menu(f"Incorrect!\nGame over!\n", options, sumPoints)            
+    else:
+        sumPoints = sumPoints + result
+        print("Congratulations! You found the book in time!")
+        print(f"\nTotal points earned so far: {sumPoints}")
     
-
-    if start(f"Ready for the next game?", yesNo) == "playerIsReady":
-        if checkForRestart(sumPoints, askRiddle("do something")) == "restart": 
-            return False
+    print("continue story(down in basement)")    
+    result = askRiddle("get an ingredient from the goblin")       
+    if result == "gameOver":
+        menu(f"Incorrect!\nGame over!\n", options, sumPoints)
+        return False
+    else:
+        sumPoints = sumPoints + result
+        print("Congratulations! You found the book in time!")
+        print(f"\nTotal points earned so far: {sumPoints}")
+    
+    print("continue story(to chest - you see a chest in the corner and the goblin alows you to open it if you get the right code. )")    
+    result = binaryCode("open the chest")
+    if result == "gameOver":
+        menu(f"Incorrect!\nGame over!\n", options, sumPoints)
+        return False
+    else:
+        sumPoints = sumPoints + result
+        print("Congratulations! You managed to open the chest")
+        print(f"\nTotal points earned so far: {sumPoints}")
+    
+    print("continue story(the fairy show up to give you more directions)")
+    result = math("get the directions ")
+    if result == "gameOver":
+        menu(f"Incorrect!\nGame over!\n", options, sumPoints)
+        return False
+    else:
+        sumPoints = result
+        print("Congratulations! You receved directions from the fairy")
+        print(f"\nTotal points earned so far: {sumPoints}")
         
-    if start(f"Ready for the next game?", yesNo) == "playerIsReady":
-        if checkForRestart(sumPoints, binaryCode("do something")) == "restart": 
-            return False
-
+    print("continue story(to the witch)")
+    result = askRiddle("get help from the witch")       
+    if result == "gameOver":
+        menu(f"Incorrect!\nGame over!\n", options, sumPoints)
+        return False
+    else:
+        sumPoints = sumPoints + result
+        print("Congratulations! The witch found you worthy to help")
+        print(f"\nTotal points earned so far: {sumPoints}")
     
-    if start(f"Ready for the next game?", yesNo) == "playerIsReady":
-        if checkForRestart(sumPoints, speedGame("do something")) == "restart": 
-            return False
+    
+    
+  #  if start(f"Ready for the next game?", yesNo) == "playerIsReady":
+         
+        
+
+  #  if start(f"Ready for the next game?", yesNo) == "playerIsReady":
+   #     result = binaryCode("do something")
+        
+    
+    
+ #   if start(f"Ready for the next game?", yesNo) == "playerIsReady":
+   #     result = speedGame("do something")   
+         
         
 while True: 
     if main(sumPoints) == False:
         sumPoints = 0
         main(sumPoints)
-        True
     
 
 
+    
+
+                
+        
+                
+        
     
 
                 
